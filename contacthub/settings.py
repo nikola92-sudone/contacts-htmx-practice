b41bc6d4c030eea913379b0e79f69174d4b9898d
Nikola Milosevic <nikola92-sudone@users.noreply.github.com>
2026-06-05 11:18:32 +1000

    Django HTMX lesson 4

--- contacthub/settings.py
+++ contacthub/settings.py
@@ -64,9 +64,13 @@ WSGI_APPLICATION = 'contacthub.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "contacthub",
+        "USER": "django_practice_user",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
--- contacts/models.py
+++ contacts/models.py
@@ -4,3 +4,19 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+        related_name='contacts'
+        )
+    
+    class Meta: 
+        unique_together = ('user', 'email')
+
+    def __str__(self) -> str:
+        return f"{self.name} <{self.email}>"
